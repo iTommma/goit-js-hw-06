4937229c72ec228c69f078c2e8d08c5d77682810
iTommma <itommma@gmail.com>
2022-12-24 00:23:53 +0200

    Завдання 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// створюэмо елемент
+const creList = ing => {
+  const element = document.createElement(`li`);
+  element.textContent = ing;
+  element.classList.add('item');
+  return element
+}
+
+// перебираэмо масив ingredients
+const result = ingredients.map( ingredient => {
+  return creList(ingredient)
+});
+
+// знаходим список
+const list = document.querySelector('#ingredients');
+
+// повертаэмо елемент
+list.append(...result)
