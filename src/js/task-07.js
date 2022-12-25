890fe3081b962c92f51c9a4ff8ad30f185136ed9
iTommma <itommma@gmail.com>
2022-12-25 18:32:00 +0200

    Завдання 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,2 +1,15 @@
 
+const fontSizeControl = document.querySelector('#font-size-control');
+const myText = document.querySelector('#text');
+// console.log(fontSizeControl);
+// console.log(text);
+
+
+const onText = () => {
+    const value = fontSizeControl.value;
+    myText.style.fontSize = value + 'px';
+}
+
+document.addEventListener('input', onText);
+
 // Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
