4d09472aaf6d276a67b15e21f7527fa9f2b7d92f
iTommma <itommma@gmail.com>
2022-12-26 16:10:29 +0200

    Завдання 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,12 +1,40 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const inputNumber = document.querySelector('#controls [type="number"]');
+const buttonCreate = document.querySelector("#controls [data-create]");
+const buttonDestroy = document.querySelector("#controls [data-destroy]");
+const divBoxes = document.querySelector("#boxes");
+// console.log(getRandomHexColor() );
 
-// ня і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисне
+let amount = 0;
+// inputNumber.addEventListener( 'input', onNumber );
+buttonCreate.addEventListener("click", onNumber);
+buttonDestroy.addEventListener("click", () => (divBoxes.innerHTML = ""));
 
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+// Перевірка на валідність
+function onNumber() {
+  amount = inputNumber.valueAsNumber;
+  if (amount < 1 || amount > 100) {
+    alert("Кількість елементів має бути від 1 до 100");
+  } else {
+    createBoxes(amount);
+  }
+}
 
-// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+// Створення колекції
+function createBoxes(amount) {
+  // console.log('createBoxes');
+
+  let allBox = "";
+  for (let i = 1; i <= amount; i += 1) {
+    // console.log(i);
+
+    const size = 20 + 10 * i;
+    allBox += `<div style="background-color: ${getRandomHexColor()}; height: ${size}px; width: ${size}px" > ${i} </div>`;
+    // console.log(allBox);
+
+    divBoxes.innerHTML = allBox;
+  }
+}
